712fd252a0d5caf34c2b3ecf72e1ccdc9363511a
EzequielGomezCucchiararo <ezequiel.cucchiararo@gmail.com>
2020-02-19 09:22:44 +0100

    character model fixed with new fields

--- models/Character.js
+++ models/Character.js
@@ -3,6 +3,8 @@ const { model, Schema } = require('mongoose');
 const characterSchema = new Schema({
     name: String,
     status: String,
+    image: String,
+    gender: String,
 });
 
 module.exports = model('Character', characterSchema);
